9c5f5d73139d9908bd00e2b88b7ef81b7c1f382b
elnurxo <elnurykh@code.edu.az>
2023-10-17 18:19:46 +0400

    js string methods

---
+++ 16. js (classes, string methods)/js-string-methods/main.js
@@ -0,0 +1,54 @@
+//TASK 1
+// let text = 'banana_apple_kiwi';
+// console.log(text.replaceAll('_','-'));
+
+// TASK 2
+// const toLower = (str) => str.toLowerCase();
+
+// console.log(toLower('HEY'));
+
+//TASK 3
+// const toArray = function(input) {
+//     return input.trim().split('');
+// }
+// console.log(toArray('   hey   '));
+
+//TASK 4
+// const LowerDashedString = (str)=> str.toLowerCase().replaceAll(' ','-');
+// console.log(LowerDashedString('Robin Singh from USA'));
+
+//TASK 5
+// function Capitalize(str) {
+//     return (str.charAt(0).toUpperCase()+str.slice(1));
+// }
+
+// console.log(Capitalize('my name is Joe'));
+
+//TASK 6
+// const CountUpperCase = function(str){
+//     let counter = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i]==str[i].toUpperCase() && str[i].trim()!='') {
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+
+// console.log(CountUpperCase('Hello   JS World')); //4
+
+//TASK 7
+// function IsFound(text,word) {
+//     let result = {
+//         IsFound: false,
+//         index: -1
+//     }
+//     if (text.toLowerCase().trim().includes(word.toLowerCase().trim())) {
+//         result.IsFound = true;
+//         result.index =  text.indexOf(word);
+//     }
+//     return result;
+// }
+// console.log(IsFound('hello world','apple'));
+
+//TASK 8
